dbe2a560b14dcdcdec235c1b2a14fa1d0d1b0a1f
im620 <im620@ic.ac.uk>
2021-06-22 10:46:44 +0530

    Homepage with black top container and round buttons for sign up and login as well as weekly challenges

--- frontend/src/components/homePage/homePage.js
+++ frontend/src/components/homePage/homePage.js
@@ -1,14 +1,18 @@
 import React from "react";
 import NavBar from "../navbar/navbar";
 import "./homePage.css";
-import { BiodiversityProfile } from "../biodiversityProfile";
-import { WeeklyChallenge } from "../biodiversityProfile";
-import { UserProfile } from "../userProfile";
-import LogIn  from "../login/login";
-import SignUp from "../signup/signup";
-import { AccountBox } from "../accountBox";
+import "./homepagestyle.css";
+// import { BiodiversityProfile } from "../biodiversityProfile";
+// import { WeeklyChallenge } from "../biodiversityProfile";
+// import { UserProfile } from "../userProfile";
+// import LogIn  from "../login/login";
+// import SignUp from "../signup/signup";
+// import { AccountBox } from "../accountBox";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+
+{/* <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;0,800;1,300;1,600&display=swap" rel="stylesheet"></link> */}
 
 const animals = [
     "bee",
@@ -21,34 +25,52 @@ const animals = [
 ];
 
 const Button = styled.button`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px; 
+  border: 0;
+  background: blanchedalmond;
+  font-weight: bold;
+  font-family: sans-serif;
+  cursor: pointer;
   color: #fff;
-  padding: 5px 15pxs
-  border-radius: 40px,
+  border-radius: 40px;
   outline: 0;
   width: 220px;
   margin: 10px;
-  cursor: pointer;
   box-shadow: 0px 2px 2px lightgray;
   &:hover{
-    background-color: #283593;
+    background-color: #000;
   }
 `;
 
 const AppContainer = styled.div`
   display: flex;
+  width: device-width;
+  height: 100%;
   flex-direction: column;
-  align-items: center;
+  align-items: column;
   justify-content: center;
-  background: orange;
+  background: white;
   color: white;
+`;
+
+const TopContainer = styled.div`
+  display: center;
+  width: device-width;
+  background: rgb(0,0,0);
 `;
 export default class HomePage extends React.Component {
     render() {
         return (
             <AppContainer>
-                <NavBar/>
-                <div className="title">
-                    <h1>BioHunters</h1>
+                <TopContainer>
+                  <NavBar/>
+                </TopContainer>
+                
+                <div class="welcome-text">
+                  <h1>Are you a <span>BioHunter</span>?</h1>
+                  <Button>LEARN MORE</Button>
                 </div>
                 {/*
                 {animals.map((animal) => (
@@ -59,7 +81,7 @@ export default class HomePage extends React.Component {
                 ))} */}
         <div>
             <center>
-            <Button style={{backgroundColor:"#FBC197"}}>
+            <Button>
                 <Link to={`/animal/bee`}>
                 Week 1: What's the buzz about?
                 </Link>
@@ -68,7 +90,7 @@ export default class HomePage extends React.Component {
       </div>
       <div>
         <center>
-        <Button style={{backgroundColor:"#fff"}}>
+        <Button style={{color:"#fff"}}>
         <Link to={`/animal/bug`}>
                 Week 2: Who may I be?
         </Link></Button>
@@ -76,7 +98,7 @@ export default class HomePage extends React.Component {
       </div>
       <div>
         <center>
-        <Button style={{backgroundColor:"#38B6FF"}}>
+        <Button style={{color:"#38B6FF"}}>
         <Link to={`/animal/bird`}>
                 Week 3: A little birdie told me...
         </Link>
@@ -85,7 +107,7 @@ export default class HomePage extends React.Component {
       </div>
       <div>
         <center>
-        <Button style={{backgroundColor:"#05ABC4"}}>
+        <Button style={{color:"#05ABC4"}}>
         <Link to={`/animal/bug`}>
                 Week 4: Are you bugged yet?
         </Link>
@@ -94,7 +116,7 @@ export default class HomePage extends React.Component {
       </div>
       <div>
         <center>
-        <Button style={{backgroundColor:"#FAD74B"}}>
+        <Button style={{color:"#FAD74B"}}>
         <Link to={`/animal/mouse`}>
                 Week 5: Why rat when you can...
         </Link>
@@ -103,7 +125,7 @@ export default class HomePage extends React.Component {
       </div>
       <div>
         <center>
-        <Button style={{backgroundColor:"#E8C892"}}>
+        <Button style={{color:"#E8C892"}}>
         <Link to={`/animal/robin`}>
                 Week 6: Will you be able to find me?
         </Link>
@@ -112,15 +134,13 @@ export default class HomePage extends React.Component {
       </div>
       <div>
         <center>
-        <Button style={{backgroundColor:"#99CC63"}}>
+        <Button style={{color:"#99CC63"}}>
         <Link to={`/animal/mushroom`}>
                 Week 7: I'm a fun-guy
         </Link>
         </Button>
         </center>
       </div>
-                <br/>    
-                <WeeklyChallenge/>
             </AppContainer>
 
         );
--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,32 +1,106 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import "./navbar.css";
+import logo from "./BioHunters.png";
+
+const Button = styled.button`
+  display: center;
+  justify-content: space-between;
+  padding: 7px 20px; 
+  border: 0;
+  float: right;
+  background: blanchedalmond;
+  font-weight: bold;
+  font-family: sans-serif;
+  cursor: pointer;
+  color: black;
+  border-radius: 40px;
+  outline: 0;
+  width: 120px;
+  margin: 7px;
+  box-shadow: 0px 2px 2px lightgray;
+  &:hover{
+    background-color: #fff;
+  }
+`;
 
 export default class NavBar extends React.Component {
     render() {
         return (
-            <div className="navbar">
+        <div class="nav-bar">
+            <div class="logo">
+              <img src={logo} alt="BioHunters Logo"></img>
+            </div>
+        <div class="nav-links" id="nav-links">
+          <i class="fa fa-close" onclick="closeMenu()"></i>
+            <ul>
+              <li>
+                <Link to="/home">
+                    Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/about">
+                    About
+                </Link>
+              </li>
+              <li>
+                <Link to="/community">
+                    Community
+                </Link>
+              </li>
+              <li>
                 <Link to="/login">
-                    <div className="button">
-                        Login
+                    Gallery
+                </Link>
+              </li>
+              <li>
+                <Link to="/login">
+                    Contact Us
+                </Link>
+              </li>
+            </ul>
+            <div>
+              <Button>LOGIN</Button>
+            </div>
+            <div>
+              <Button>SIGN UP</Button>
+            </div>
+          </div>
+          <i class="fa fa-bars" onclick="showMenu()"></i>
+        </div>
+            
+        );
+    }
+}
+
+/* <ul>
+                <div className="nav-bar">
+                <Link to="/login">
+                    <div class="button">
+                        Home
                     </div>
                 </Link>
                 <Link to="/signup">
-                    <div className="button">
-                        Sign up
+                    <div class="button">
+                        About
                     </div>
                 </Link>
                 <Link to="/about">
-                    <div className="button">
-                        About
+                    <div class="button">
+                        Community
                     </div>
                 </Link>
                 <Link to="/profile">
                     <div className="button">
-                        Profile
+                        Gallery
+                    </div>
+                </Link>
+                <Link to="/profile">
+                    <div className="button">
+                        Contact
                     </div>
                 </Link>
             </div>
-        );
-    }
-}
+            </ul> */
